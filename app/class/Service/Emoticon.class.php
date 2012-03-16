77d2cd518fc27bdf7a0348f14a181854accd0b18
Pierre <ph_7@hotmail.be>
2012-03-16 09:10:52 +0100

    Small change

--- app/class/Service/Emoticon.class.php
+++ app/class/Service/Emoticon.class.php
@@ -9,57 +9,71 @@
  * @license     CC-BY - http://creativecommons.org/licenses/by/3.0/
  * @version     $Id: Emoticon.class.php 2012-02-21 pierrehs $
  */
- 
+
 namespace PH7\Framework\Service;
 defined('PH7') or exit('Restricted access');
 
 class Emoticon {
-	
-	const DIR = 'smile/', EXT = '.gif';
-	
-	
-	/**
-	 * @desc Gets the list of emoticons
-	 * @return array
-	 */
-	public static function gets() {
-		return include PH7_PATH_APP . 'config/emoticon.php';
-	}
-	
-	/**
-	 * @desc Gets the path of emoticon
-	 * @param string $sName
-	 * @return emoticon path
-	 */
-	public static function getPath($sName) {
-		return PH7_PATH_STATIC . PH7_IMG . self::DIR . $sName . self::EXT;
-	}
 
-	/**
-	 * @desc Gets the URL of emoticon
-	 * @param string $sName
-	 * @return emoticon URL
-	 */
-	public static function getUrl($sName) {
-		return PH7_URL_STATIC . PH7_IMG . self::DIR . $sName . self::EXT;
-	}
-	
-	/**
-	 * @desc Gets the name of emoticon
-	 * @param array $aVal
-	 * @return emoticon name
-	 */
-	public static function getName($aVal) {
-		return $aVal[1];
-	}
+    const DIR = 'smile/', EXT = '.gif';
 
-	/**
-	 * @desc Gets the emoticon code
-	 * @param array $aVal
-	 * @return emoticon code
-	 */
-	public static function getCode($aVal) {
-		return $aVal[0];
-	}
-	
+
+    /**
+     * @constructor
+     * @desc Private constructor to prevent instantiation of class since it is a private class.
+     * @access private
+     */
+    private function __construct() {}
+
+    /**
+     * @desc Gets the list of emoticons
+     * @return array
+     */
+    public static function gets() {
+        return include PH7_PATH_APP . 'config/emoticon.php';
+    }
+
+    /**
+     * @desc Gets the path of emoticon
+     * @param string $sName
+     * @return emoticon path
+     */
+    public static function getPath($sName) {
+        return PH7_PATH_STATIC . PH7_IMG . self::DIR . $sName . self::EXT;
+    }
+
+    /**
+     * @desc Gets the URL of emoticon
+     * @param string $sName
+     * @return emoticon URL
+     */
+    public static function getUrl($sName) {
+        return PH7_URL_STATIC . PH7_IMG . self::DIR . $sName . self::EXT;
+    }
+
+    /**
+     * @desc Gets the name of emoticon
+     * @param array $aVal
+     * @return emoticon name
+     */
+    public static function getName($aVal) {
+        return $aVal[1];
+    }
+
+    /**
+     * @desc Gets the emoticon code
+     * @param array $aVal
+     * @return emoticon code
+     */
+    public static function getCode($aVal) {
+        return $aVal[0];
+    }
+
+    /**
+     * @clone
+     * @desc Block cloning
+     * @access private
+     */
+    private function __clone() {}
+
 }
